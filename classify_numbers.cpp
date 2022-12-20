4f7b5e9f01a44f1b7b18f791026a21077adb46bd
Marshal <marshalkalynchuk1@gmail.com>
2022-12-19 18:53:58 -0700

    Added feedforward

--- classify_numbers.cpp
+++ classify_numbers.cpp
@@ -8,15 +8,15 @@ int main(){
 
   int num_inputs = 3;
   int num_outputs = 2;
-  int num_hidden_layers = 1;
+  int num_hidden_layers = 2;
   int num_nodes_per_hidden_layer[2] = {2, 3};
 
   NeuralNetwork nn(num_inputs, num_outputs, num_hidden_layers, num_nodes_per_hidden_layer);
-
   double input[3] = {1.0, 2.0, 3.0};
   double* result = nn.feedforward(input);
-  for (int i = 0; i < num_inputs; i++) {
+  for (int i = 0; i < num_outputs; i++) {
     std::cout << result[i] << std::endl;
   }
+
   return 0;
 }
--- neural_network.cpp
+++ neural_network.cpp
@@ -1,6 +1,13 @@
 
 #include <vector>
 #include <random>
+#include <math.h>
+#include <iostream>
+#include <istream>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <string.h>
 
 using namespace std;
 
@@ -11,17 +18,20 @@ class NeuralNetwork {
       num_inputs_(num_inputs),
       num_outputs_(num_outputs),
       num_hidden_layers_(num_hidden_layers),
-      num_nodes_per_hidden_layer_(num_nodes_per_hidden_layer) {
+      num_nodes_per_hidden_layer_(num_nodes_per_hidden_layer){
 
-      num_layers_ = num_hidden_layers_ + 1;
+      if (num_nodes_per_hidden_layer == nullptr) {
+        std::cerr << "Error: num_nodes_per_hidden_ is nullptr" << std::endl;
+        exit(1);
+      }
 
-      weights_ = new double**[num_layers_];
-      biases_ = new double*[num_layers_];
+      int num_layers = num_hidden_layers_ + 1;
 
-      activations_ = new double*[num_layers_];
-      errors_ = new double*[num_layers_];
+      weights_ = new double**[num_layers];
+      biases_ = new double*[num_layers];
 
-      for (int i = 0; i < num_layers_; i++) {
+      for (int i = 0; i < num_layers; i++) {
+
         int num_inputs;
         int num_outputs;
         if (i == 0) {
@@ -31,31 +41,52 @@ class NeuralNetwork {
           // Other layers have numNodesPerHiddenLayer[i-1] inputs
           num_inputs = num_nodes_per_hidden_layer_[i - 1];
         }
-        if (i == num_layers_ - 1) {
+        if (i == num_layers - 1) {
           // Output layer has numOutputs outputs
           num_outputs = num_outputs_;
         } else {
           // Other layers have numNodesPerHiddenLayer[i] outputs
           num_outputs = num_nodes_per_hidden_layer_[i];
         }
+
         // Initialize the weights and biases for this layer
-        weights_[i] = new double*[num_inputs];
-        biases_[i] = new double[num_inputs];
-        for (int j = 0; j < num_inputs; j++) {
-          weights_[i][j] = new double[num_outputs];
-          for (int k = 0; k < num_outputs; k++){
+        weights_[i] = new double*[num_outputs];
+        biases_[i] = new double[num_outputs];
+
+        for (int j = 0; j < num_outputs; j++) {
+          weights_[i][j] = new double[num_inputs];
+          for (int k = 0; k < num_inputs; k++){
             weights_[i][j][k] = randomValue(-0.5, 0.5);
           }
-          biases_[i][j] = randomValue(0.5, 0.5);
+          biases_[i][j] = randomValue(-0.5, 0.5);
+        }
+        
+      }
+      activations_ = new double*[num_layers + 1];
+      errors_ = new double*[num_layers + 1];
+      for (int i = 0; i < num_layers + 1; i++){
+        int num_inputs;
+        int num_outputs;
+        if (i == 0) {
+          // First layer has num_inputs nodes
+          num_inputs = num_inputs_;
+        } else if (i == num_layers - 1) {
+          // Last layer has num_outputs nodes
+          num_outputs = num_outputs_;
+        } else {
+          // Other layers have num_nodes_per_hidden_layer[i-1] nodes
+          num_inputs = num_nodes_per_hidden_layer_[i - 1];
         }
         // Initialize the activation and errors for this layer
-        activations_[i] = new double[num_inputs];
-        errors_[i] = new double[num_inputs];
+        activations_[i] = new double[num_outputs];
+        errors_[i] = new double[num_outputs];
       }
     }
+
     // Destructor
     ~NeuralNetwork(){
-      for (int i = 0; i < num_layers_; i++){
+      int num_layers = num_hidden_layers_ + 1;
+      for (int i = 0; i < num_layers; i++){
         int num_inputs;
         int num_outputs;
         if (i == 0) {
@@ -65,16 +96,15 @@ class NeuralNetwork {
           // Other layers have numNodesPerHiddenLayer[i-1] inputs
           num_inputs = num_nodes_per_hidden_layer_[i - 1];
         }
-        if (i == num_layers_ - 1) {
+        if (i == num_layers - 1) {
           // Output layer has numOutputs outputs
           num_outputs = num_outputs_;
         } else {
           // Other layers have numNodesPerHiddenLayer[i] outputs
           num_outputs = num_nodes_per_hidden_layer_[i];
         }
-        for (int j = 0; j < num_inputs; j++) {
+        for (int j = 0; j < num_outputs; j++) 
           delete[] weights_[i][j];
-        }
         delete[] biases_[i];
         delete[] weights_[i];
         delete[] activations_[i];
@@ -88,7 +118,47 @@ class NeuralNetwork {
 
     // Feedforward method
     double* feedforward(double* inputs){
-
+      if (inputs == nullptr){
+        std::cerr << "Error: Input is null" << std::endl;
+        exit(1);
+      }
+      int num_layers = num_hidden_layers_ + 1;
+      // Set the activations of the input layer to the input values
+      for (int i = 0; i < num_inputs_; i++) {
+        activations_[0][i] = inputs[i];
+      }
+      // Loop over the layers of the network
+      for (int i = 0; i < num_layers; i++) {
+        int num_inputs;
+        int num_outputs;
+        if (i == 0) {
+          // First layer has numInputs inputs
+          num_inputs = num_inputs_;
+        } else {
+          // Other layers have numNodesPerHiddenLayer[i-1] inputs
+          num_inputs = num_nodes_per_hidden_layer_[i - 1];
+        }
+        if (i == num_layers - 1) {
+          // Output layer has numOutputs outputs
+          num_outputs = num_outputs_;
+        } else {
+          // Other layers have numNodesPerHiddenLayer[i] outputs
+          num_outputs = num_nodes_per_hidden_layer_[i];
+        }
+        // Calculate the weighted sum of the inputs for each node in the next layer
+        for (int j = 0; j < num_outputs; j++) {
+          double weighted_sum = 0.0;
+          for (int k = 0; k < num_inputs; k++) {
+            weighted_sum += activations_[i][k] * weights_[i][j][k];
+          }
+          // Add the bias of the node
+          weighted_sum += biases_[i][j];
+          // Calculate the activation of the node using the sigmoid function
+          activations_[i + 1][j] = 1.0 / (1.0 + exp(-weighted_sum));
+        }
+      }
+      // Return the activations of the output layer
+      return activations_[num_layers - 1];
     }
 
     // Backpropagation method
@@ -102,8 +172,6 @@ class NeuralNetwork {
       return dist(gen);
     }
   private:
-
-    int num_layers_;
     int num_inputs_;
     int num_outputs_;
     int num_hidden_layers_;
